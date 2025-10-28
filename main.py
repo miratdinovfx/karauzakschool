b3f0b757db92afe6d03f327bb685c02773a7696e
miratdinovfx <miratdinovfx@gmail.com>
2025-10-28 21:19:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import telebot
+import os
 from flask import Flask, request
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 API_TOKEN = "8041913948:AAFn4ujzHM1ovTNPnpOuguOV7mCnHGK0zGo"  # bu yerga bot tokenini yoz
 
